10d55a045c4caa505a2a7df35380b7470952720f
EvanBG42 <evanbg42@gmail.com>
2018-09-20 09:29:57 -0400

    starting GALP example1

--- testPrograms/helloWorld.asm
+++ testPrograms/helloWorld.asm
@@ -1,5 +1,11 @@
 ; Hello World on GBC
-; Evan Gambill
+; Evan Gambill - 20 Sep. 2018
 ; Following GALP example1
 
-INCLUDE "gbhw.inc"
+INCLUDE "gbhw.inc" ; Hardware defines
+INCLUDE "ibmpc1.inc" ; Font macro
+
+SECTION "Org $100", HOME[$100] ; $100 is code execution starting addr.
+
+nop
+jp	begin
